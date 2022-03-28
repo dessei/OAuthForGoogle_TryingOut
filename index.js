ca74c53d10d6946b541d0d50d6183ac4de417ed1
dessei <Fritz.Meitner@gmx.de>
2022-03-29 01:56:44 +0200

    add prompt

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 window.onload = function () {
-    myclient = google.accounts.id.initialize({
-      client_id: "787718893823-ief5upq817odgurk45ga691p9m3ejd57.apps.googleusercontent.com",
-      callback: handleCredentialResponse
-    });
-    google.accounts.id.renderButton(
-      document.getElementById("buttonDiv"),
-      { theme: "outline", size: "large" }  // customization attributes
-    )}
+        google.accounts.id.initialize({
+        client_id: "787718893823-ief5upq817odgurk45ga691p9m3ejd57.apps.googleusercontent.com",
+        callback: handleCredentialResponse
+        });
+        google.accounts.id.renderButton(
+        document.getElementById("buttonDiv"),
+        { theme: "outline", size: "large" }  // customization attributes
+        )
+        google.accounts.id.prompt()
+    }
