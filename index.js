ccf0356ff52b5713439078ef6d5d0d60d8c54e6e
dessei <Fritz.Meitner@gmx.de>
2022-03-29 02:15:21 +0200

    trying how hard i inted

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 window.onload = function () {
         google.accounts.id.initialize({
-        client_id: "721028352816-l9a1rdbuie529l8pmimtgo0bkmnt56bb.apps.googleusercontent.com",
-        callback: handleCredentialResponse
-        });
-        google.accounts.id.renderButton(
-        document.getElementById("buttonDiv"),
-        { theme: "outline", size: "large" }  // customization attributes
-        )
-        google.accounts.id.prompt()
+            client_id: "721028352816-l9a1rdbuie529l8pmimtgo0bkmnt56bb.apps.googleusercontent.com",
+            callback: handleCredentialResponse
+            });
+            google.accounts.id.renderButton(
+                document.getElementById("buttonDiv"),
+                { theme: "outline", size: "large" }  // customization attributes
+            )
+            google.accounts.id.prompt()
     }
